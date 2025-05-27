7b6585bff0d8d5aea58ca2f48b5226677c95183a
tranphat180603 <tranphat180603@gmail.com>
2025-05-27 09:19:04 +0700

    finish

--- ops_posthog/slack_reporter.py
+++ ops_posthog/slack_reporter.py
@@ -130,8 +130,7 @@ class SlackReporter:
                 logger.info(f"Sending weekly report for {dashboard_name} to channel {channel_id}")
                 
                 # Send the report to the appropriate channel
-                # await self.send_message(channel_id, report + "\n\n" + "@channel")
-                print("Temporarily disabled sending to slack")
+                await self.send_message(channel_id, report + "\n\n" + "@channel")
                 print(f"Weekly report for {dashboard_name} sent to {channel_id}")
                 logger.info(f"Weekly report for {dashboard_name} sent to {channel_id}")
 
